f451bcb6f7ad63f43f8c914f9fdfe763463e23cc
nekich77792 <nekit77792@gmail.com>
2019-06-17 18:04:32 +0300

    дщд

--- scripts/my.js
+++ scripts/my.js
@@ -1,17 +1,2 @@
-function init ()
-{
-	//console.log ("ага была загрузка");
+console.log(напиши число!!!!);
 
-	var myRadio = document.getElementById('r1');
-	if (myRadio)
-	{
-		myRadio.addEventListener ( "change" , foo);
-	}
-}
-
-
-
-function foo ( e )
-{
-	alert("Hello man");
-}
