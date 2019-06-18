32e858113c0bdb46be63c188538f67e5c989240d
nekich77792 <nekit77792@gmail.com>
2019-06-18 16:26:28 +0300

    nikita

--- scripts/my.js
+++ scripts/my.js
@@ -1,6 +1,6 @@
 console.log("Напиши число!");
 
-document.getElementById("0w0").onclick = potato() 
+document.getElementById("0w0").onclick = function() 
 {
 	 document.getElementById("0w0").innerHTML = ".w.";
 
